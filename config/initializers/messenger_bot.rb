137741302c4ea75689f06aaca43973b019fe8d0e
Juan Carlos Santiago <krlos013@gmail.com>
2016-05-29 00:19:45 -0500

    2

--- config/initializers/messenger_bot.rb
+++ config/initializers/messenger_bot.rb
@@ -1,5 +1,5 @@
 Messenger::Bot.config do |config|
-  config.access_token = <EAAWmSTe58XEBABkg6SEwF8KPlf1ZBSoJLtt9e7RpaTZBjcUsd26F6oZAoZC82ZAWuoaav2C6ZCfVNr4VfmuAoYZAEVUWhBhHdhcBdi6O8OojUFR85nn1yAyhlahYkvXoHO6A56g8aWRzmd1FpiLwQ8hy1JaImc3ZALUhjnZBJ5FVNggZDZD>
-  config.validation_token = <VERIFY_TOKEN>
-  config.secret_token = <5bfc23e1ef50051509a42875fd584a30>
+  config.access_token = "EAAWmSTe58XEBABkg6SEwF8KPlf1ZBSoJLtt9e7RpaTZBjcUsd26F6oZAoZC82ZAWuoaav2C6ZCfVNr4VfmuAoYZAEVUWhBhHdhcBdi6O8OojUFR85nn1yAyhlahYkvXoHO6A56g8aWRzmd1FpiLwQ8hy1JaImc3ZALUhjnZBJ5FVNggZDZD"
+  config.validation_token = "VERIFY_TOKEN"
+  config.secret_token = "5bfc23e1ef50051509a42875fd584a30"
 end
