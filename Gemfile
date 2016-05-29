f651b75ac1c2cd809648d535ed4a38ce83bcb27c
Juan Carlos Santiago <krlos013@gmail.com>
2016-05-29 00:15:13 -0500

    2

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'messenger-bot'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'sqlite3'
 end
 
 group :development do
--- config/initializers/messenger_bot.rb
+++ config/initializers/messenger_bot.rb
@@ -1,5 +1,5 @@
 Messenger::Bot.config do |config|
-  config.access_token = "EAAWmWxuZCLW0BANYMrlZAN45cwHn0Ihvm0uPc3eRyyoJH3xa3nxAXmfBaNWPeU9ysX3KrtrKZBceBop0f9fypALXLTQzzbJFCGzQAVA12sCinJsDAC8xfDtiLJKPaP98QDjs42qjbfkxpsX8ZCJZAvwwIeYjgRwXXEXvUblPRpwZDZD"
-  config.validation_token = "1590285121301869"
-  config.secret_token = "ce720508f18ef72fd2955954ab337388"
+  config.access_token = <EAAWmSTe58XEBABkg6SEwF8KPlf1ZBSoJLtt9e7RpaTZBjcUsd26F6oZAoZC82ZAWuoaav2C6ZCfVNr4VfmuAoYZAEVUWhBhHdhcBdi6O8OojUFR85nn1yAyhlahYkvXoHO6A56g8aWRzmd1FpiLwQ8hy1JaImc3ZALUhjnZBJ5FVNggZDZD>
+  config.validation_token = <VERIFY_TOKEN>
+  config.secret_token = <5bfc23e1ef50051509a42875fd584a30>
 end
