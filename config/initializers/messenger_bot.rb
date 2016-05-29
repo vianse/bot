19aae066dbf201091a789fd48b0f801820e01d52
Juan Carlos Santiago <krlos013@gmail.com>
2016-05-28 23:45:41 -0500

    first commit

---
+++ config/initializers/messenger_bot.rb
@@ -0,0 +1,5 @@
+Messenger::Bot.config do |config|
+  config.access_token = "EAAWmWxuZCLW0BANYMrlZAN45cwHn0Ihvm0uPc3eRyyoJH3xa3nxAXmfBaNWPeU9ysX3KrtrKZBceBop0f9fypALXLTQzzbJFCGzQAVA12sCinJsDAC8xfDtiLJKPaP98QDjs42qjbfkxpsX8ZCJZAvwwIeYjgRwXXEXvUblPRpwZDZD"
+  config.validation_token = "1590285121301869"
+  config.secret_token = "ce720508f18ef72fd2955954ab337388"
+end
